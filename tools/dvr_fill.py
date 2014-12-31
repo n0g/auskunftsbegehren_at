37cb83c9e8f2bcda2c9cda3bf2b103be2d10f597
Matthias Fassl <mf@n0g.at>
2014-12-31 15:51:04 +0100

    caught parsing exception

--- tools/dvr_fill.py
+++ tools/dvr_fill.py
@@ -12,7 +12,10 @@ for ag in Auftraggeber.objects.all():
     if ag_apps == None:
         continue
     for ag_app in ag_apps:
-        dt = datetime.datetime.strptime(ag_app[2],'%d.%m.%Y')
+        try:
+            dt = datetime.datetime.strptime(ag_app[2],'%d.%m.%Y')
+        except:
+            continue
         new_date = "{0}-{1}-{2}".format(dt.year,dt.month,dt.day)
         aam = Application(number=ag_app[0],description=ag_app[1],date=new_date,state=ag_app[3])
         aam.auftraggeber = ag
